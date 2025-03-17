1f42d32c92a77ff88a8c660d410bc09841daaaeb
tanzeelak <tnkhan8042@gmail.com>
2025-03-16 19:25:51 -0700

    update api

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 
 export const loginUser = async (username: string) => {
-  return axios.post(`${API_BASE_URL}/login`, { username });
+  return axios.post(`${API_BASE_URL}/login`, { "name": username });
 };
 
 export const logoutUser = async () => {
